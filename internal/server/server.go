ee36cf0fc875ef991b75143efdd0530792deecd0
rasatmaja <rasioatmaja29@gmail.com>
2021-08-07 20:29:05 +0700

    feat(server): install and core config fiber

---
+++ internal/server/server.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// App is struct that define server, repo, and all component app needs
+type App struct {
+	server *fiber.App
+}
+
+// New is a function to initialize sever and its component
+func New() *App {
+
+	// setup server
+	svr := fiber.New(
+		fiber.Config{
+			UnescapePath: true,
+		},
+	)
+
+	return &App{
+		server: svr,
+	}
+}
+
+// Start is a function to start server
+func (a *App) Start() {
+
+	a.InitializeShutdownSequence()
+	defer fmt.Println("[ SRVR ] Server Shutdown...")
+
+	a.ServerListen()
+
+}
+
+// ServerListen is a function to initialize server listen
+func (a *App) ServerListen() {
+
+	var err error
+	host := fmt.Sprintf("%s:%d", "localhost", 9000)
+
+	err = a.server.Listen(host)
+
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ internal/server/shutdown.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+)
+
+// InitializeShutdownSequence is a function initialize shutdown sequence
+func (a *App) InitializeShutdownSequence() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		fmt.Println(" :: OS signal received")
+		fmt.Println("[ SRVR ] Gracefully shutting down...")
+		fmt.Println("[ SRVR ] Running cleanup tasks...")
+
+		a.server.Shutdown()
+	}()
+}
