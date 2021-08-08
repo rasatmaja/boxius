cefe55d54c09746177545daf9c76f769bc01716b
rasatmaja <rasioatmaja29@gmail.com>
2021-08-08 13:42:29 +0700

    feat(server): add timeout config

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,27 +2,37 @@ package server
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/rasatmaja/boxius/internal/config"
 )
 
 // App is struct that define server, repo, and all component app needs
 type App struct {
 	server *fiber.App
+	env    *config.ENV
 }
 
 // New is a function to initialize sever and its component
 func New() *App {
 
+	// load env
+	env := config.LoadENV()
+
 	// setup server
 	svr := fiber.New(
 		fiber.Config{
+			ReadTimeout:  time.Duration(env.ServerReadTO) * time.Second,
+			WriteTimeout: time.Duration(env.ServerWriteTO) * time.Second,
+			IdleTimeout:  time.Duration(env.ServerIdleTO) * time.Second,
 			UnescapePath: true,
 		},
 	)
 
 	return &App{
 		server: svr,
+		env:    env,
 	}
 }
 
@@ -41,7 +51,7 @@ func (a *App) Start() {
 func (a *App) ServerListen() {
 
 	var err error
-	host := fmt.Sprintf("%s:%d", "localhost", 9000)
+	host := fmt.Sprintf("%s:%d", a.env.ServerHost, a.env.ServerPort)
 
 	err = a.server.Listen(host)
 
